74882533d48df7366a0dc412c64be5d372698e26
Theo Vora <theo.vora@gmail.com>
2022-12-03 20:06:04 -0500

    create model

---
+++ DaysOfTheWeek/Models/Day.swift
@@ -0,0 +1,18 @@
+//
+//  Day.swift
+//  DaysOfTheWeek
+//
+//  Created by Theo Vora on 12/3/22.
+//
+
+import Foundation
+
+class Day {
+    let name: String
+    let origin: String
+    
+    init(name: String, origin: String) {
+        self.name = name
+        self.origin = origin
+    }
+}
