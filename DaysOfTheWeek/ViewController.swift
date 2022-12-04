b2d4660077875f5489262f6f42f9d9c655ff9d22
Theo Vora <theo.vora@gmail.com>
2022-12-03 20:00:23 -0500

    Initial Commit

---
+++ DaysOfTheWeek/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DaysOfTheWeek
+//
+//  Created by Theo Vora on 12/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("hello there")
+    }
+
+
+}
+
